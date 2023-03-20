5fcefb1e56aa53dd8491a22b648846a746c82845
Korotaevam <kam888@mail.ru>
2023-03-20 15:05:28 +0300

    before migration

--- app_shop_orm_drf/shop_main/models.py
+++ app_shop_orm_drf/shop_main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class ShopModels(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название Магазина')
+    city = models.ForeignKey('CityModels', on_delete=models.PROTECT, verbose_name='Город')
+    street = models.ForeignKey('StreetModels', on_delete=models.PROTECT, verbose_name='Улица')
+    house = models.CharField(max_length=255, verbose_name='Дом')
+    time_open = models.TimeField(verbose_name='время открытия')
+    time_close = models.TimeField(verbose_name='время закрытия')
+
+
+class CityModels(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название Города')
+
+
+class StreetModels(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название Улицы')
+    city = models.ForeignKey('CityModels', on_delete=models.PROTECT, verbose_name='Город')
+
